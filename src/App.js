b9590f1e6309201f9308af37576d1f43cb60a2e3
linsonsimon <linsonsimon2@gmail.com>
2024-02-15 18:56:02 +0530

    basic mailing skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [subject, setSubject] = useState("");
+  const [text, setText] = useState("");
+
+  const sendMail = async () => {
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+      },
+    };
+    console.log(from, to, subject, text);
+    const { data } = await axios.post(
+      "http://localhost:5000/api/v1/mail",
+      {
+        to: to,
+        subject: subject,
+        from: from,
+        text: text,
+      },
+      config
+    );
+
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        from :<input value={from} onChange={(e) => setFrom(e.target.value)} />
+      </div>
+      <div>
+        to :<input value={to} onChange={(e) => setTo(e.target.value)} />
+      </div>
+      <div>
+        subject :
+        <input value={subject} onChange={(e) => setSubject(e.target.value)} />
+      </div>
+      <div>
+        text :<input value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+
+      <button
+        onClick={() => {
+          sendMail();
+        }}
+      >
+        sendMail
+      </button>
     </div>
   );
 }
