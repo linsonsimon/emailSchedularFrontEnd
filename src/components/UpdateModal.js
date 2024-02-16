259eb440dbd2e7379f9f643e3da676bb89db9f86
linsonsimon <linsonsimon2@gmail.com>
2024-02-16 22:26:45 +0530

    modal css added

--- src/components/UpdateModal.js
+++ src/components/UpdateModal.js
@@ -45,83 +45,161 @@ const UpdateModal = ({ setShowModal, mailData }) => {
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
-        backgroundColor: "red",
         alignItems: "center",
+        justifyContent: "center",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        height: "100vh",
+        width: "100vw",
+        background: "rgba(179, 181, 186, 0.5)",
       }}
     >
-      <button onClick={() => setShowModal(false)}>close</button>
-      <div className="compose-form">
-        <label>
-          From:
-          <input
-            type="text"
-            name="from"
-            value={from}
-            onChange={(e) => setFrom(e.target.value)}
-          />
-        </label>
-        <label>
-          To:
-          <div style={{ display: "flex", gap: "10px" }}>
-            {to.map((email, index) => (
-              <button
-                key={index}
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTo(to.filter((x) => x !== email));
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "80%",
+          width: "60%",
+          gap: "10px",
+          padding: "5px 5px",
+          backgroundColor: "whitesmoke",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "25px",
+            justifyContent: "space-between",
+          }}
+        >
+          <p>Reshedule Mail</p>
+          <button
+            onClick={() => setShowModal(false)}
+            style={{ height: "25px", width: "25px" }}
+          >
+            X
+          </button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            justifyContent: "space-between",
+            rowGap: "5px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              rowGap: "5px",
+            }}
+          >
+            <label
+              style={{
+                borderWidth: "0.5px 0 0 0",
+                borderStyle: "solid",
+                padding: "5px 0",
+                gap: "5px",
+              }}
+            >
+              From:
+              <input
+                style={{ padding: "0 5px " }}
+                type="text"
+                name="from"
+                value={from}
+                onChange={(e) => setFrom(e.target.value)}
+              />
+            </label>
+            <label>
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  rowGap: "2px",
+                  gap: "2px 5px",
+                  borderWidth: "0.5px 0 0 0",
+                  borderStyle: "solid",
+                  padding: "5px 0",
                 }}
               >
-                {email}
-              </button>
-            ))}
-            <input
-              type="text"
-              name="to"
-              value={tempTo}
-              onChange={(e) => setTempTo(e.target.value)}
-              onBlur={() => {
-                tempTo.trim() !== "" && setTo((prev) => [...prev, tempTo]);
-                setTempTo("");
-              }}
-              onKeyPress={(e) => {
-                if (e.key == "Enter") {
-                  tempTo.trim() !== "" && setTo((prev) => [...prev, tempTo]);
-                  setTempTo("");
-                }
-              }}
-            />
+                To:
+                {to.map((email, index) => (
+                  <p style={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}>
+                    {email + "  "}
+
+                    <button
+                      key={index}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setTo(to.filter((x) => x !== email));
+                      }}
+                      style={{
+                        height: "90%",
+                        borderRadius: "40%",
+                        borderWidth: 0,
+                      }}
+                    >
+                      X
+                    </button>
+                  </p>
+                ))}
+                <input
+                  type="text"
+                  name="to"
+                  value={tempTo}
+                  onChange={(e) => setTempTo(e.target.value)}
+                  onBlur={() => {
+                    tempTo.trim() !== "" && setTo((prev) => [...prev, tempTo]);
+                    setTempTo("");
+                  }}
+                  onKeyPress={(e) => {
+                    if (e.key == "Enter") {
+                      tempTo.trim() !== "" &&
+                        setTo((prev) => [...prev, tempTo]);
+                      setTempTo("");
+                    }
+                  }}
+                  style={{ borderWidth: 0 }}
+                />
+              </div>
+            </label>
+            <label>
+              Subject:
+              <input
+                type="text"
+                name="subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
+            </label>
+            <label>
+              date:
+              <input
+                type="datetime-local"
+                name="date"
+                min={new Date()}
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </label>
           </div>
-        </label>
-        <label>
-          Subject:
-          <input
-            type="text"
-            name="subject"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-          />
-        </label>
-        <label>
-          Content:
+
           <textarea
             value={content}
+            placeholder="content"
             onChange={(e) => setContent(e.target.value)}
+            style={{ height: "80%" }}
           />
-        </label>
-
-        <label>
-          date:
-          <input
-            type="datetime-local"
-            name="date"
-            min={new Date()}
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </label>
-
-        <button onClick={reScheduledMail}>submit</button>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <button>attach</button>
+            <button onClick={reScheduledMail}>submit</button>
+          </div>
+        </div>
       </div>
     </div>
   );
